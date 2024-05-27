660160c7cfce7bd122d8a44d6c5c3e3bc95ba55f
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2024-05-27 14:58:33 +0200

    Update hometask

---
+++ controllers/avatarUserControllers.js
@@ -0,0 +1,60 @@
+import { User } from "../models/users.js";
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import Jimp from "jimp";
+import HttpError from "../helpers/HttpError.js";
+
+async function getAvatar(req, res, next) {
+  try {
+    const userAvatar = await User.findById(req.user.id);
+      if (!userAvatar) {
+        throw HttpError(404, "User not found");
+    }
+      if (userAvatar.avatarURL=== undefined) {
+        throw HttpError(404, "Avatar not found");
+      }
+      const pathResolve = res.sendFile(path.resolve("public/avatars", userAvatar.avatarURL), (error) => {
+          if (error) {
+              throw HttpError(404, "Avatar not found");
+          }
+      });
+  } catch (error) {
+    next(error);
+  }
+}
+async function uploadAvatar(req, res, next) {
+  try {
+    if (!req.file) {
+     throw HttpError(400,  "No file uploaded");
+      }
+      const filePath = req.file.path;
+      let avatar;
+      try {
+    avatar = await Jimp.read(filePath);
+      } catch (error) {
+          throw HttpError(400, "Failed to process image");
+      }
+      avatar.resize(250, 250).write(filePath);
+      try {
+         await fs.rename(
+           req.file.path,
+           path.resolve("public/avatars", req.file.filename)
+         );
+      } catch (error) {
+          return res.status(500).json({ message: "Failed to move image" });
+      }
+
+    const userAvatar = await User.findByIdAndUpdate(
+      req.user.id,
+      { avatarURL: req.file.filename },
+      { new: true }
+    );
+      if (userAvatar === null) {
+        throw HttpError(400, "User not found");
+    }
+    res.status(200).json( {avatarURL:  userAvatar.avatarURL});
+  } catch (error) {
+    next(error);
+  }
+}
+export default { getAvatar, uploadAvatar };
