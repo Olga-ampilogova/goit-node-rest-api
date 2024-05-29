286b11d6dc18a89382a875e019ab2e52e5a01e17
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2024-05-29 14:29:57 +0200

    Changing in the avatarUserControllers

--- controllers/avatarUserControllers.js
+++ controllers/avatarUserControllers.js
@@ -3,17 +3,18 @@ import * as fs from "node:fs/promises";
 import path from "node:path";
 import Jimp from "jimp";
 import HttpError from "../helpers/HttpError.js";
+import { fileURLToPath } from "node:url";
 
 async function getAvatar(req, res, next) {
   try {
     const userAvatar = await User.findById(req.user.id);
-      if (!userAvatar) {
-        throw HttpError(404, "User not found");
+    if (!userAvatar) {
+      throw HttpError(404, "User not found");
     }
-      if (!userAvatar.avatarURL) {
-        throw HttpError(404, "Avatar not found");
-      }
-    res.sendFile(path.resolve("public/avatars", userAvatar.avatarURL));
+    if (!userAvatar.avatarURL) {
+      throw HttpError(404, "Avatar not found");
+    }
+    res.sendFile(path.resolve("public", userAvatar.avatarURL));
   } catch (error) {
     next(error);
   }
@@ -23,33 +24,36 @@ async function uploadAvatar(req, res, next) {
     if (!req.file) {
      throw HttpError(400,  "No file uploaded");
       }
-      const filePath = req.file.path;
+    const filePath = req.file.path;
+  
       let avatar;
       try {
     avatar = await Jimp.read(filePath);
       } catch (error) {
           throw HttpError(400, "Failed to process image");
       }
-      avatar.resize(250, 250).write(filePath);
-      try {
+    avatar.resize(250, 250).write(filePath);
+    let resolvedPath = path.resolve("public/avatars", req.file.filename);
+    try {
          await fs.rename(
            req.file.path,
-           path.resolve("public/avatars", req.file.filename)
-         );
+           resolvedPath
+      );
+      resolvedPath = path.relative("public", resolvedPath);
       } catch (error) {
           return res.status(500).json({ message: "Failed to move image" });
       }
 
     const userAvatar = await User.findByIdAndUpdate(
       req.user.id,
-      { avatarURL: req.file.filename },
+      { avatarURL: resolvedPath },
       { new: true }
     );
       if (userAvatar === null) {
         throw HttpError(400, "User not found");
     }
-    res.status(200).json( {avatarURL:  userAvatar.avatarURL});
-  } catch (error) {
+    res.status(200).json({ avatarURL: userAvatar.avatarURL });
+      } catch (error) {
     next(error);
   }
 }
